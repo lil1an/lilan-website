481623287cb6dda16fe83b4517cfe9ef4f7153b8
Lilan <lilan0213@yahoo.com>
2025-03-01 17:37:36 -0500

    feat(ui): create waiting page for deployment setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
 import NavBar from './components/NavBar.vue'
+import Header from './sections/Header.vue'
 </script>
 
 <template>
-  <NavBar> </NavBar>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="flex items-center justify-center h-screen bg-blue-100">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold text-gray-700">Lilan's Website</h1>
+      <p class="text-2xl text-gray-500 mt-2">Coming Soon...</p>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+
+<style>
+</style>
